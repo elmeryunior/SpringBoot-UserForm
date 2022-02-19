a2f87cf68c5510aa1284f361abd36bddf8292d3c
usuario <usuario@Palma-52803293>
2022-02-19 15:18:35 -0500

    Se crea una BD con los roles: ADMIN ROLE_ADMIN USER ROLE_USER SUPERVISOR ROLE_SUPERVISOR

---
+++ UserForm/src/main/java/com/userform/service/UserService.java
@@ -0,0 +1,24 @@
+package com.userform.service;
+
+import javax.validation.Valid;
+
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.userform.Exception.UsernameOrIdNotFound;
+import com.userform.dto.ChangePasswordForm;
+import com.userform.entity.User;
+
+public interface UserService {
+
+	public Iterable<User> getAllUsers();
+
+	public User createUser(User user) throws Exception;
+
+	public User getUserById(Long id) throws Exception;
+	
+	public User updateUser(User user) throws Exception;
+	
+	public void deleteUser(Long id) throws UsernameOrIdNotFound;
+	
+	public User changePassword(ChangePasswordForm form) throws Exception;
+}
